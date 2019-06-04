7821f43cac6835287e2e98d502d902321c0af239
Christhopher Lion <christhopherleao@icloud.com>
2019-06-04 16:59:59 +0100

    fix json response

--- handlers/people.js
+++ handlers/people.js
@@ -1,2 +1,10 @@
-exports.profile = (req, res) => res.json(require.resolve('../data/people/christhopher_lion/profile.json'));
-exports.social = (req, res) => res.json(require.resolve('../data/people/christhopher_lion/social.json'));
+const profile = require('../data/people/christhopher_lion/profile.json');
+const social = require('../data/people/christhopher_lion/social.json');
+
+const lion = {
+  profile,
+  social,
+};
+
+exports.profile = (req, res) => res.json(lion.profile);
+exports.social = (req, res) => res.json(lion.social);
